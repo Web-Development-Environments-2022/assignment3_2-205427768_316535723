efb11a8b33f372a01a4a0b40ac63d55a482c8970
mayasosnovik <mayasos@post.bgu.ac.il>
2022-06-10 10:54:56 +0300

    update recipe utils

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,19 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+/**
+ * This path returns 3 random preview recipes
+ */
+router.get("/random", async (req, res, next) =>{
+  try{
+    let random_3_recipes = await recipes_utils.getRandomThreeRecipes();
+    console.log(random_3_recipes);
+    res.send(random_3_recipes);
+  }
+  catch(error){
+    next(error);
+  }
+});
 
 /**
  * This path returns a full details of a recipe by its id
@@ -12,6 +25,18 @@ router.get("/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * This path returns a full details of a recipe by its id
+ */
+ router.get("/complexSearch/:dishName", async (req, res, next) => {
+  try {
+    const Recipes_search_15 = await recipes_utils.searchRecipes(req.params.dishName);
+    res.send(Recipes_search_15);
   } catch (error) {
     next(error);
   }
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+//const { exports } = require("mssql/lib/base");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -18,10 +19,49 @@ async function getRecipeInformation(recipe_id) {
     });
 }
 
+async function getRecipesPreview(recipe_ids_list){
+    let promises = [];
+    recipe_ids_list.map((id) =>{
+        promises.push(getRecipeInformation(id));
+    });
+    let info_res = await Promise.all(promises);
+    return extractPreviewRecipeDetails(info_res);
+}
+/**
+ * Get recipes list of lenth 15 from spooncular response (serach)
+ * @param {*} dish_Name 
+ */
+async function searchRecipeInformation(dish_Name){
+    console.log(dish_Name)
+    const response = await axios.get(`${api_domain}/complexSearch`,{
+        params:{
+            number: 15,
+            query: dish_Name,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    console.log(response)
+    return response;
+}
 
 
-async function getRecipeDetails(recipe_id) {
-    let recipe_info = await getRecipeInformation(recipe_id);
+
+/**
+ * Get random recipes list of length 10 from spooncular
+ * @param {*} 
+ */
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random`,{
+        params:{
+            Number: 10,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    return response;
+}
+
+function extractPreviewRecipeDetails(recipe_info){
+  /*  console.log(recipe_info)
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
 
     return {
@@ -34,12 +74,137 @@ async function getRecipeDetails(recipe_id) {
         vegetarian: vegetarian,
         glutenFree: glutenFree,
         
+    }*/
+    return recipe_info.map((recipe_info) =>{
+        let data = recipe_info;
+        if(recipe_info.data!= null){
+            data = recipe_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,
+    
+        }
+    }) 
+}
+
+/*async function getRecipeDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree,  } = recipe_info.data;
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        
+    }
+}*/
+async function getRecipeDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, extendedIngredients, instructions, servings} = recipe_info.data;
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        extendedIngredients: extendedIngredients,
+        instructions: instructions,
+        servings: servings,
     }
 }
 
+async function searchRecipes(dish_Name){
+    let recipe_info = await searchRecipeInformation(dish_Name);
+    console.log(recipe_info.lenth);
 
 
+
+
+  /*  const response = await axios.get(`${api_domain}/informationBulk`,{
+        params:{
+            ids: [recipe_info.data.results[0], recipe_info.data.results[1], recipe_info.data.results[2]],
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    console.log(response);*/
+    /*let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, extendedIngredients, instructions, servings} = recipe_info.data;
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        extendedIngredients: extendedIngredients,
+        instructions: instructions,
+        servings: servings,
+    }*/
+
+    /*let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, extendedIngredients, instructions, servings} = response.data[0];
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        extendedIngredients: extendedIngredients,
+        instructions: instructions,
+        servings: servings,
+    }*/
+
+
+    let {id, title} = recipe_info.data.results[0];
+    return {
+        id: id,
+        title: title,
+    }
+}
+
+async function getRandomThreeRecipes(){
+    let random_pool = await getRandomRecipes();
+    console.log("length of pool");
+    let filtered_random_pool = random_pool.data.recipes.filter((random) =>(random.instructions != "") && (random.image && random.instructions)) //??
+    if(filtered_random_pool.lenth < 3){
+        return getRandomThreeRecipes();
+    }
+    console.log(filtered_random_pool[0])
+    return extractPreviewRecipeDetails([filtered_random_pool[0], filtered_random_pool[1], filtered_random_pool[2]]);
+}
 exports.getRecipeDetails = getRecipeDetails;
-
-
+exports.getRecipesPreview = getRecipesPreview;
+exports.getRandomThreeRecipes = getRandomThreeRecipes;
+exports.searchRecipes = searchRecipes;
 
