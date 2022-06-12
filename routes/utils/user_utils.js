b35d55795836b0a30e312dbdb18c284f7b5d6d2b
mayasosnovik <mayasos@post.bgu.ac.il>
2022-06-12 14:25:16 +0300

    last viewed recipes

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -13,8 +13,14 @@ async function markAsViewed(user_id, recipe_id){
     await DButils.execQuery(`insert into recipesviews values ('${user_id}','${recipe_id}', now())`);
 }
 
+async function getLastWatchedRecipes(user_id){
+    const recipes_id = await DButils.execQuery(`select recipe_id from recipesviews where user_id='${user_id}' AND recipe_id = (select recipe_id from recipesviews as alt WHERE alt.recipe_id = recipesviews.recipe_id ORDER BY date DESC LIMIT 1) ORDER BY date DESC LIMIT 3`);
+    return recipes_id;
+}
+
 
 
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.markAsViewed = markAsViewed;
+exports.getLastWatchedRecipes = getLastWatchedRecipes;
